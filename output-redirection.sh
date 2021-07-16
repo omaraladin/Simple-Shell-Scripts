aa23ae8da984a83ec18c93b9a3e2cd349bb45441
Omar Aladdin <omar.alaeldain@gmail.com>
2021-07-16 14:24:44 +0200

    Added Rdirection Script

---
+++ output-redirection.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Redirects output but with WRITE ">" Mode
+echo "Hello World" > redirect-script-with-write.log
+
+#Redirects output but with Append ">>" Mode
+echo "This will be Appended not Overwrited" >> redirect-script-with-append.log
+
+exit
