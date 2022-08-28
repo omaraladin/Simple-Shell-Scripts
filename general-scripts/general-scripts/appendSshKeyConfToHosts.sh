b34e1368e87dfce454056b2425cd0784e035a4f9
Omar Aladdin <48786827+omaraladin@users.noreply.github.com>
2022-08-28 15:40:16 +0400

    Create appendSshKeyConfToHosts.sh

---
+++ general-scripts/general-scripts/appendSshKeyConfToHosts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script assumes that the configured ssh keys are owned by a user named ansible
+# and keys location is in /home/ansible/.ssh/id_hostName_rsa
+while IFS= read -r -d $'\n' -u 9
+do
+    #If line starts with "[" then it means it is an AnsibleHostGroup definition 
+    #so prints a new line from the last ansibleHostGroupHosts and echos the line AS IT IS
+	if [[ $REPLY == [* ]]
+	then
+		echo -e "\n$REPLY"
+
+    #If line starts with "#" then it means it is a comment 
+    #so echos the line AS IT IS
+	elif [[ $REPLY == \#* ]]
+	then
+		echo "$REPLY" #>> final.txt;
+
+    #If above conditions are not met then this is the line which starts with a host ALIAS
+    #so append the string to the line, see stringAppend variable
+	else
+#		echo $REPLY		
+#		echo -e "Modified to:"
+		nodeName=$(echo $REPLY | awk '{ print $1 }')
+		stringAppend="ansible_ssh_private_key_file=/home/ansible/.ssh/id_${nodeName}_rsa"
+		echo "$REPLY    $stringAppend"
+	fi
+
+#The ssh-unconfigured inventory file should be given as an input
+done 9< inventory2.txt
